16ba294cd5424239c37c66640ce56a9f89637193
choiyoungwoo <mariakjs96@naver.com>
2025-05-04 20:02:48 +0900

    robots modify

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
+// next-sitemap.config.js
+
 module.exports = {
   siteUrl: "https://www.energyplan-kr.com",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    additionalRobotsTxt: `
+  #DaumWebMasterTool:d6a9eafc838c7b4a8b2aa6cb32bfd15106b291a46f6d58f642c83a0000ac5f92:Yzosx6SjVqxInf4teZmdGw==
+      `,
+  },
 };
