7ab1ee7d26c131a82e9cf96e388cc775e5ff3d69
choiyoungwoo <mariakjs96@naver.com>
2025-05-04 20:05:12 +0900

    robots modify

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -11,8 +11,6 @@ module.exports = {
         allow: "/",
       },
     ],
-    additionalRobotsTxt: `
-  #DaumWebMasterTool:d6a9eafc838c7b4a8b2aa6cb32bfd15106b291a46f6d58f642c83a0000ac5f92:Yzosx6SjVqxInf4teZmdGw==
-      `,
+    additionalRobotsTxt: `#DaumWebMasterTool:d6a9eafc838c7b4a8b2aa6cb32bfd15106b291a46f6d58f642c83a0000ac5f92:Yzosx6SjVqxInf4teZmdGw==`,
   },
 };
