631538eac4691f867fb40fc45e7d481c7b1ba08e
choiyoungwoo <mariakjs96@naver.com>
2025-05-04 19:49:48 +0900

    daum search

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.energyplan-kr.com", // 배포된 실제 URL로 바꾸세요
+  siteUrl: "https://www.energyplan-kr.com",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: ["https://www.energyplan-kr.com/sitemap.xml"],
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    // ✅ 다음 인증 주석 추가
+    additionalRobotsTxt: `
+  #DaumWebMasterTool:d6a9eafc838c7b4a8b2aa6cb32bfd15106b291a46f6d58f642c83a0000ac5f92:Yzosx6SjVqxInf4teZmdGw==
+      `,
+  },
 };
