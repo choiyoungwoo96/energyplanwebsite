62015ddd545f2a39cd9d05cb3892640a2f361233
choiyoungwoo <mariakjs96@naver.com>
2025-05-07 01:43:44 +0900

    profit page

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import CommunitySection from "@/components/Section/communitySection";
 import ConstructionCases from "@/components/Section/constructionCases";
 import DirectionSection from "@/components/Section/directionSection";
 import Footer from "@/components/Section/footer";
-import Profit from "@/components/Section/profit";
+
 import QAAccordion from "@/components/Section/QAAccordion";
 import Service from "@/components/Section/service";
 import Sidemenu from "@/components/Section/sidemenu";
@@ -18,7 +18,6 @@ export default function Home() {
       <Sidemenu></Sidemenu>
       <Header></Header>
       <Slide></Slide>
-      <Profit></Profit>
       <About></About>
       <Business></Business>
       <CommunitySection></CommunitySection>
---
+++ src/app/profit/page.jsx
@@ -0,0 +1,4 @@
+import Profit from "@/components/Section/profit";
+export default function page() {
+  return <Profit></Profit>;
+}
